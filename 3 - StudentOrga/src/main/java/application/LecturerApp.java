62a3a0276b70e21e1bfd76216b4f97cd71e61633
gabrielakbarov <kontakt@gabrielakbarov.de>
2025-05-12 20:57:11 +0200

    3 - Vererbung : Minor changes

--- 3 - StudentOrga/src/main/java/application/LecturerApp.java
+++ 3 - StudentOrga/src/main/java/application/LecturerApp.java
@@ -2,9 +2,21 @@ package application;
 
 import model.Lecturer;
 
+import java.util.Scanner;
+
 public class LecturerApp{
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         Lecturer lecturer = new Lecturer("Holger", "Parlow", 1970, "Paderborn", "Wirtschaftsinformatik");
         System.out.println(lecturer.info());
+
+        Lecturer lecturer2 = new Lecturer();
+        lecturer2.newInput(scanner);
+        lecturer2.setDepartment("Datenbanken");
+        lecturer2.setCity("Berlin");
+        System.out.println(lecturer2.getCity());
+        System.out.println(lecturer2.getDepartment());
+        System.out.println(lecturer2.info());
     }
 }
--- 3 - StudentOrga/src/main/java/application/StudentApp.java
+++ 3 - StudentOrga/src/main/java/application/StudentApp.java
@@ -6,5 +6,10 @@ public class StudentApp {
     public static void main(String[] args) {
         Student student = new Student("Nick", "Schwabenland", 2006, "Paderborn", "Angewandte Informatik");
         System.out.println(student.info());
+
+        Student student2 = new Student();
+        student2.setSubject("Business English");
+        System.out.println(student2.getSubject());
+        System.out.println(student2.info());
     }
 }
